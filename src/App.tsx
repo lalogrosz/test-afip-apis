870187b04b591ec8c5bf9739f0954f57c67f81c4
Alan Grosz <lalogrosz@gmail.com>
2019-09-04 08:30:26 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
+import { LoginTicket, Wsfev1 } from "afip-apis";
 import './App.css';
+
+
+
+const DEFAULT_URLWSAAWSDL: string = "https://wsaahomo.afip.gov.ar/ws/services/LoginCms?WSDL";
+const DEFAULT_SERVICIO: string = "wsfe";
+const DEFAULT_CERTIFICATE: string = "./certificate.crt";
+const DEFAULT_CERTIFICATE_KEY: string = "./private.key";
+
+
+const loginTicket: LoginTicket = new LoginTicket();
+loginTicket.wsaaLogin(DEFAULT_SERVICIO, DEFAULT_URLWSAAWSDL, DEFAULT_CERTIFICATE, DEFAULT_CERTIFICATE_KEY)
+  .then(r => {
+    console.log(r.header);
+    const wsfev1: Wsfev1 = new Wsfev1("https://wswhomo.afip.gov.ar/wsfev1/service.asmx?WSDL");
+    return wsfev1.FEDummy({})
+      .then(d => {
+        console.log(d);
+      });
+  })
+  .catch(e => console.error(e));
+
 
 const App: React.FC = () => {
   return (
